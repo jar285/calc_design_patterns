088017902b970f5409c4267b705b865badf84ee6
Jesus <jar285@njit.edu>
2024-02-22 20:23:26 -0500

    fixed test and command pattern

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class Command(ABC):
     @abstractmethod
-    def execute(self, **kwargs):
+    def execute(self):
         pass
 
 class CommandHandler:
@@ -12,8 +12,8 @@ class CommandHandler:
     def register_command(self, command_name: str, command: Command):
         self.commands[command_name] = command
 
-    def execute_command(self, command_name: str, **kwargs):
+    def execute_command(self, command_name: str):
         if command_name in self.commands:
-            self.commands[command_name].execute(**kwargs)
+            self.commands[command_name].execute()
         else:
             print(f"No such command: {command_name}")
