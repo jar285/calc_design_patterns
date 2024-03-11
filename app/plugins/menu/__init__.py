cd3372ad389e73c9cc84fb1418ea6c79d27071e2
Jesus <jar285@njit.edu>
2024-03-10 23:12:25 -0400

    imported logs to menu

--- app/plugins/menu/__init__.py
+++ app/plugins/menu/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from app.commands import Command
 
 class MenuCommand(Command):
@@ -5,6 +6,10 @@ class MenuCommand(Command):
         self.command_handler = command_handler
 
     def execute(self):
+        # Log the action of displaying the menu
+        logging.info("Displaying available commands to the user.")
+        
         print("Available commands:")
         for command_name in self.command_handler.list_commands():
+            # Keeping print statements for direct user interaction
             print("-", command_name)
