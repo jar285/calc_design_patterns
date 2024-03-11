4ce6622cb79a4ccd9a39983e1537c13858d941c7
Jesus <jar285@njit.edu>
2024-03-10 23:10:20 -0400

    exit plugin has logs imported

--- app/plugins/exit/__init__.py
+++ app/plugins/exit/__init__.py
@@ -1,7 +1,10 @@
 import sys
+import logging
 from app.commands import Command
-
 
 class ExitCommand(Command):
     def execute(self):
+        # Log the intention to exit the application
+        logging.info("Exiting the application upon user request.")
+        # Exit the application
         sys.exit("Exiting...")
